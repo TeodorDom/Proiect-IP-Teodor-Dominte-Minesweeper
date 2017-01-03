8b2e2029489afc1b8d107c37762d1b707eac570b
TeodorDom <claw_teodor@yahoo.com>
2017-01-04 01:45:56 +0200

    Commit 3
    Codul APROAPE gata pentru player. Needs debugging + tratarea unor cazuri
    particulare.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int directieLin[]={-1,-1,-1,0,0,1,1,1},directieCol[]={-1,0,1,-1,1,-1,0,1};
 bool jocTerminat=0;
+unsigned int nrMine,deschise=0;
 
 struct matrice
 {
@@ -13,13 +14,7 @@ struct matrice
     int val[51][51];
 }tablaJoc;
 
-char afisaj[51][51];
-
-unsigned int randomizare(matrice a)
-{
-    srand(time(0));
-    return (rand()%a.linii + 1);
-}
+unsigned char afisaj[51][51];
 
 unsigned int numarMine(unsigned int linie, unsigned int coloana, matrice a)
 {
@@ -36,19 +31,20 @@ unsigned int numarMine(unsigned int linie, unsigned int coloana, matrice a)
     return mine;
 }
 
-void construiesteMatrice(unsigned int nrMine)
+void construiesteMatrice()
 {
     unsigned int i,j,linie,coloana;
+    srand(time(0));
     for (i=1;i<=nrMine;i++)
     {
-        linie=randomizare(tablaJoc);
-        coloana=randomizare(tablaJoc);
+        linie=rand()%tablaJoc.linii+1;
+        coloana=rand()%tablaJoc.linii+1;
         tablaJoc.val[linie][coloana]=-1;
     }
     for (i=1;i<=tablaJoc.linii;i++)
         for (j=1;j<=tablaJoc.linii;j++)
         {
-            afisaj[i][j]=char(254);
+            afisaj[i][j]=254;
             if (tablaJoc.val[i][j]!=-1)
                 tablaJoc.val[i][j]=numarMine(i,j,tablaJoc);
         }
@@ -58,7 +54,7 @@ void deschide(unsigned int linie, unsigned int coloana)
 {
     if (tablaJoc.val[linie][coloana]==-1)
     {
-        afisaj[linie][coloana]=char(15);
+        afisaj[linie][coloana]=15;
         jocTerminat=1;
         cout<<"AI PIERDUT !";
     }
@@ -71,11 +67,15 @@ void deschide(unsigned int linie, unsigned int coloana)
             coloanaNoua=coloana+directieCol[i];
             if (linieNoua>=1 && linieNoua<=tablaJoc.linii && coloanaNoua>=1 && coloanaNoua<=tablaJoc.linii)
             {
-                if (tablaJoc.val[linieNoua][coloanaNoua]!=-1)
+                if (tablaJoc.val[linieNoua][coloanaNoua]!=-1 &&  afisaj[linieNoua][coloanaNoua]==254)
                 {
+                    ++deschise;
                     afisaj[linieNoua][coloanaNoua]=tablaJoc.val[linieNoua][coloanaNoua]+'0';
                     if (tablaJoc.val[linieNoua][coloanaNoua]==0)
+                    {
+                        afisaj[linieNoua][coloanaNoua]=' ';
                         deschide(linieNoua,coloanaNoua);
+                    }
                 }
             }
         }
@@ -85,16 +85,57 @@ void deschide(unsigned int linie, unsigned int coloana)
 void afisare()
 {
     int i,j;
+    system("cls");
     for (i=1;i<=tablaJoc.linii;i++)
     {
         for (j=1;j<=tablaJoc.linii;j++)
             cout<<afisaj[i][j]<<" ";
         cout<<endl;
     }
+    cout<<deschise;
+}
+
+void citire()
+{
+    cout<<"Nr. linii :\n";cin>>tablaJoc.linii;
+    cout<<"Nr. mine , max "<<(tablaJoc.linii-1)*(tablaJoc.linii-1)<<" :\n";
+    cin>>nrMine;
+    while (nrMine>(tablaJoc.linii-1)*(tablaJoc.linii-1))
+    {
+        cout<<"Numarul de mine este prea mare. Introduceti din nou \n";
+        cin>>nrMine;
+    }
+}
+
+void joc()
+{
+    char raspuns;
+    unsigned int i,linie,coloana;
+    citire();
+    construiesteMatrice();
+    for (i=1;i<=tablaJoc.linii-nrMine && !jocTerminat && deschise!=tablaJoc.linii-nrMine;)
+    {
+        afisare();
+        cout<<"Pozitia : linie coloana\n";
+        cin>>linie>>coloana;
+        cout<<"Deschizi/pui steag/scoti steag? d/f/u";
+        cin>>raspuns;
+        if (raspuns=='d'||raspuns=='D')
+        {
+            deschide(linie,coloana);
+            i++;
+        }
+        else if (raspuns=='f'||raspuns=='F')
+            afisaj[linie][coloana]=239;
+        else
+            afisaj[linie][coloana]=254;
+    }
+    if (deschise==tablaJoc.linii-nrMine)
+        cout<<"AI CASTIGAT !";
 }
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    joc();
     return 0;
 }
